67638b2b6bb74fe76db03ba1a1b72e1df78d226d
Diego Farias <fariasdb@outlook.com>
2021-11-30 11:30:24 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/diegobfarias/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.diegobfarias.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.diegobfarias.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+}
