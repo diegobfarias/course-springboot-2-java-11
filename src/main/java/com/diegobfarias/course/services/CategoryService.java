e541f3c8879d1d4c6f9dcb85a53be8bbd4c064fa
Diego Farias <fariasdb@outlook.com>
2021-12-01 07:56:46 -0300

    Category

---
+++ src/main/java/com/diegobfarias/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.diegobfarias.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegobfarias.course.entities.Category;
+import com.diegobfarias.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
